d0d7f3fc947344dcf2ff6b31f1c1cc1f96b6561c
Divya0901 <gujjukadivya0109@gmail.com>
2022-11-23 00:21:03 +0530

    first commit

---
+++ src/main/java/com/ad/ad_university/AdUniversityApplication.java
@@ -0,0 +1,13 @@
+package com.ad.ad_university;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdUniversityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdUniversityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ad/ad_university/AdUniversityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ad.ad_university;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdUniversityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
